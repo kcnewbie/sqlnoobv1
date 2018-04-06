30a4fc733e910f5d28989aadabfae13e912c8866
kcnewbie <hidayatampanzz@gmail.com>
2018-04-05 22:15:20 -0500

    Update sqlscan.sh

--- sqlscan.sh
+++ sqlscan.sh
@@ -10,7 +10,7 @@ echo -e "--------------------------------"
 
 case $1 in
 	-f) cat $2| for i in $(cat $2); do nmap -sV --script=http-sql-injection $i; done;;
-	-s) read $2 | nmap -sV --script=http-sql-injection $2 ;;
+	-s) $2 | nmap -sV --script=http-sql-injection $2 ;;
 
 esac
 
