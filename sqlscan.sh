a63cc5b89f75e7b399769204db1a030ef9302cb1
kcnewbie <hidayatampanzz@gmail.com>
2018-04-05 19:15:28 -0500

    Add files via upload

---
+++ sqlscan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#coded by kcnewbie
+
+echo -e "SqlNoob v.1 Scanner"
+echo -e " -------------------------------"
+echo -e "| usage: ./sqlscan.sh           "
+echo -e "--------------------------------"
+
+
+case $1 in
+	-f) cat $2| for i in $(cat $2); do nmap -sV --script=http-sql-injection $i; done;;
+	-s) read $2 | nmap -sV --script=http-sql-injection $2 ;;
+
+esac
+
